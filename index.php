04a0384d044627ab00420b97dcdede2b436e48cc
nolanritchie <nolan.ritchie@gmail.com>
2018-02-02 16:59:11 -0500

    Interview test fixes
    fixed the problems i found without actually re-writting

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-
 class Interview
 {
-	public $title = 'Interview test';
+	public static $title = 'Interview test';  // was missing the word "static" 
 }
 
 $lipsum = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus incidunt, quasi aliquid, quod officia commodi magni eum? Provident, sed necessitatibus perferendis nisi illum quos, incidunt sit tempora quasi, pariatur natus.';
@@ -15,7 +14,7 @@ $people = array(
 	array('id'=>5, 'first_name'=>'Doug', 'last_name'=>'Simons', 'email'=>'doug.simons@hotmail.com')
 );
 
-$person = $_POST['person'];
+$person = $_POST['person']; // unsafe, no form of validation before outputting 
 
 ?>
 
@@ -34,16 +33,16 @@ $person = $_POST['person'];
 
 	<?php
 	// Print 10 times
-	for ($i=10; $i<0; $i++) {
-		echo '<p>'+$lipsum+'</p>';
+	for ($i=0; $i<10; $i++) { // 2 problems with loop condition cuasing it not to run at all 
+		echo '<p>'.$lipsum.'</p>'; // changed two "+" to "." as that is how to concat string not "+" 
 	}
 	?>
 
 
 	<hr>
 
-
-	<form method="get" action="<?=$_SERVER['REQUEST_URI'];?>">
+	
+	<form method="post" action=""> <?php /*changed get to post as post was used first above 5*/       /*$_SERVER['REQUEST_URI'] in form unsafe and uneeded, changed to action="" #9*/?>
 		<p><label for="firstName">First name</label> <input type="text" name="person[first_name]" id="firstName"></p>
 		<p><label for="lastName">Last name</label> <input type="text" name="person[last_name]" id="lastName"></p>
 		<p><label for="email">Email</label> <input type="text" name="person[email]" id="email"></p>
@@ -51,7 +50,7 @@ $person = $_POST['person'];
 	</form>
 
 	<?php if ($person): ?>
-		<p><strong>Person</strong> <?=$person['first_name'];?>, <?=$person['last_name'];?>, <?=$person['email'];?></p>
+		<p><strong>Person</strong> <?=$person['first_name'];?>, <?=$person['last_name'];?>, <?=$person['email']; // unsafe, no form of validation before outputting ?></p>
 	<?php endif; ?>
 
 
@@ -67,11 +66,11 @@ $person = $_POST['person'];
 			</tr>
 		</thead>
 		<tbody>
-			<?php foreach ($people as $person): ?>
+			<?php foreach ($people as $person): // "$person" varable is already in use, though it does not cause problem now it could easily create confusion or problems if this page expands in future ?>
 				<tr>
-					<td><?=$person->first_name;?></td>
-					<td><?=$person->last_name;?></td>
-					<td><?=$person->email;?></td>
+					<td><?=$person['first_name']; // changed => to $person['first_name'] bc stored as array not object ?></td>
+					<td><?=$person['last_name']; //stored as array not object ?></td>
+					<td><?=$person['email']; // stored as array not object ?></td>
 				</tr>
 			<?php endforeach; ?>
 		</tbody>
